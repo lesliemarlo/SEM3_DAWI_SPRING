7a6aa6913237e57bc529af9457ae3eaabda1a7f9
lesliemarlo <lesliemarlo09@gmail.com>
2024-04-06 12:56:43 -0500

    dawi commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AddAlumnoComponent } from './components/add-alumno/add-alumno.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {   path: "addModalidad", component: AddAlumnoComponent  }
+];
---
+++ src/app/components/add-alumno/add-alumno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-alumno',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-alumno.component.html',
+  styleUrl: './add-alumno.component.css'
+})
+export class AddAlumnoComponent {
+
+}
